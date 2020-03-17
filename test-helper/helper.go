055ba24dfd31d4ba54500b836c0c416e88e4a5e4
a.kuznetsov <a.kuznetsov@solarl.ru>
2020-03-17 13:26:26 +0300

    add helper function

--- test-helper/helper.go
+++ test-helper/helper.go
@@ -5,7 +5,6 @@ import (
 	"github.com/SolarLabRU/fastpay-go-commons/helpers"
 	"github.com/golang/protobuf/proto"
 	"github.com/hyperledger/fabric-chaincode-go/shim"
-	"github.com/hyperledger/fabric-chaincode-go/shimtest"
 	"github.com/hyperledger/fabric-protos-go/msp"
 	"github.com/hyperledger/fabric-protos-go/peer"
 	"github.com/stretchr/testify/assert"
@@ -13,10 +12,10 @@ import (
 )
 
 const (
-	invokeType   = "INVOKE"
-	initType     = "INIT"
-	queryType    = "QUERY"
-	standardTxID = "1234567890"
+	InvokeType   = "INVOKE"
+	InitType     = "INIT"
+	QueryType    = "QUERY"
+	StandardTxID = "1234567890"
 )
 
 func CallContractFunctionAndCheckError(t *testing.T, mockStub *MockStub, arguments []string, callType string, expectedMessage string) {
@@ -38,14 +37,14 @@ func CallContractFunctionAndCheckResponse(t *testing.T, mockStub *MockStub, argu
 
 	var response peer.Response
 
-	if callType == initType {
-		response = mockStub.MockInit(standardTxID, args)
-	} else if callType == invokeType {
-		response = mockStub.MockInvoke(standardTxID, args)
-	} else if callType == queryType {
-		response = mockStub.MockInvoke(standardTxID, args)
+	if callType == InitType {
+		response = mockStub.MockInit(StandardTxID, args)
+	} else if callType == InvokeType {
+		response = mockStub.MockInvoke(StandardTxID, args)
+	} else if callType == QueryType {
+		response = mockStub.MockInvoke(StandardTxID, args)
 	} else {
-		panic(fmt.Sprintf("Call type passed should be %s or %s. Value passed was %s", initType, invokeType, callType))
+		panic(fmt.Sprintf("Call type passed should be %s or %s. Value passed was %s", InitType, InvokeType, callType))
 	}
 
 	expectedResponse := shim.Success([]byte(expectedMessage))
@@ -57,7 +56,7 @@ func CallContractFunctionAndCheckResponse(t *testing.T, mockStub *MockStub, argu
 	assert.Equal(t, expectedResponse, response)
 }
 
-func SetCreator(t *testing.T, stub *shimtest.MockStub, mspID string, idbytes []byte) {
+func SetCreator(t *testing.T, stub *MockStub, mspID string, idbytes []byte) {
 	sid := &msp.SerializedIdentity{Mspid: mspID, IdBytes: idbytes}
 	b, err := proto.Marshal(sid)
 	if err != nil {
