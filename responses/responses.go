942f0fba9cd7efe39f51e3b0b17289771b697e8d
a.kuznetsov <a.kuznetsov@solarl.ru>
2020-03-19 17:15:17 +0300

    fix SenderAddressData

--- responses/responses.go
+++ responses/responses.go
@@ -20,36 +20,22 @@ type AccountListResponse struct {
 	BaseResponse
 }
 
-//type AccountPageData struct {
-//	Metadata peer.QueryResponseMetadata `json:"metadata"`
-//	Items    []models.Account           `json:"items"`
-//}
-//
-//type AccountPageResponse struct {
-//	Data AccountPageData `json:"data"`
-//	BaseResponse
-//}
-//
-//type SenderAddressData struct {
-//	Address string `json:"address"`
-//}
-//
-//type SenderAddressResponse struct {
-//	Data SenderAddressData `json:"data"`
-//	BaseResponse
-//}
-
+// TODO Если вынести внутрению струкутра Data будет ошибка валидации Cannot use metadata. Metadata did not match schema:
+// 1. components.schemas..required: Array must have at least 1 items [recovered]
+// При создании чейнкода аккаунтов в тесте
 type AccountPageResponse struct {
 	Data struct {
 		Metadata peer.QueryResponseMetadata `json:"metadata"`
 		Items    []models.Account           `json:"items"`
-	}
+	} `json:"data"`
 	BaseResponse
 }
 
+type SenderAddressData struct {
+	Address string `json:"address"`
+}
+
 type SenderAddressResponse struct {
-	Data struct {
-		Address string `json:"address"`
-	} `json:"data"`
+	Data SenderAddressData `json:"data"`
 	BaseResponse
 }
