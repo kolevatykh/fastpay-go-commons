3b3157e9a23ed0ae8e7928a1cf3e681aec5aad0c
a.kuznetsov <a.kuznetsov@solarl.ru>
2020-03-19 11:21:12 +0300

    add AccountPageResponse

--- responses/responses.go
+++ responses/responses.go
@@ -1,18 +1,31 @@
 package responses
 
-import "github.com/SolarLabRU/fastpay-go-commons/models"
+import (
+	"github.com/SolarLabRU/fastpay-go-commons/models"
+	"github.com/hyperledger/fabric-protos-go/peer"
+)
 
 type BankResponse struct {
-	Data models.Bank
+	Data models.Bank `json:"data"`
 	BaseResponse
 }
 
 type AccountResponse struct {
-	Data models.Account
+	Data models.Account `json:"data"`
 	BaseResponse
 }
 
 type AccountListResponse struct {
-	Data []models.Account
+	Data []models.Account `json:"data"`
 	BaseResponse
+}
+
+type AccountPageResponse struct {
+	Data AccountDataPageResponse `json:"data"`
+	BaseResponse
+}
+
+type AccountDataPageResponse struct {
+	Metadata peer.QueryResponseMetadata `json:"metadata"`
+	Items    []models.Account           `json:"items"`
 }
