b6dcd3b8b3f465bf745882e0149d88afc7084c36
a.kuznetsov <a.kuznetsov@solarl.ru>
2020-03-19 16:15:27 +0300

    fix validation

--- requests/update_state_request.go
+++ requests/update_state_request.go
@@ -3,6 +3,6 @@ package requests
 import "github.com/SolarLabRU/fastpay-go-commons/enums/state_enum"
 
 type UpdateStateRequest struct {
-	Address string           `json:"address" validate:"required, validHex40"`
+	Address string           `json:"address" validate:"required,validHex40"`
 	State   state_enum.State `json:"state" validate:"required"`
 }
