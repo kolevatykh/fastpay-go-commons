fab2cb907d3520178ccdf50314816238c6a0cd10
a.kuznetsov <a.kuznetsov@solarl.ru>
2020-03-19 17:09:35 +0300

    fix SenderAddressData

--- responses/responses.go
+++ responses/responses.go
@@ -20,21 +20,36 @@ type AccountListResponse struct {
 	BaseResponse
 }
 
-type AccountPageData struct {
-	Metadata peer.QueryResponseMetadata `json:"metadata"`
-	Items    []models.Account           `json:"items"`
-}
+//type AccountPageData struct {
+//	Metadata peer.QueryResponseMetadata `json:"metadata"`
+//	Items    []models.Account           `json:"items"`
+//}
+//
+//type AccountPageResponse struct {
+//	Data AccountPageData `json:"data"`
+//	BaseResponse
+//}
+//
+//type SenderAddressData struct {
+//	Address string `json:"address"`
+//}
+//
+//type SenderAddressResponse struct {
+//	Data SenderAddressData `json:"data"`
+//	BaseResponse
+//}
 
 type AccountPageResponse struct {
-	Data AccountPageData `json:"data"`
+	Data struct {
+		Metadata peer.QueryResponseMetadata `json:"metadata"`
+		Items    []models.Account           `json:"items"`
+	}
 	BaseResponse
 }
 
-type SenderAddressData struct {
-	Address string `json:"address"`
-}
-
 type SenderAddressResponse struct {
-	Data SenderAddressData `json:"data"`
+	Data struct {
+		Address string `json:"address"`
+	} `json:"data"`
 	BaseResponse
 }
