6b246c305b0071b4204ec0b53bd68bd8b4f4e09e
a.kuznetsov <a.kuznetsov@solarl.ru>
2020-03-25 12:58:32 +0300

    fix GetAccountBalanceRequest, add currency code

--- requests/get_account_balance_request.go
+++ requests/get_account_balance_request.go
@@ -1,5 +1,6 @@
 package requests
 
 type GetAccountBalanceRequest struct {
-	Address string `json:"address" validate:"required,validHex40"`
+	Address      string `json:"address" validate:"required,validHex40"`
+	CurrencyCode int    `json:"currencyCode" validate:"required,min=0"`
 }
