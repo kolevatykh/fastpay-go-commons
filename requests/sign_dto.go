ed411d8a29c21d41f84fc7201b9a615c2da8bd61
a.kuznetsov <a.kuznetsov@solarl.ru>
2020-04-16 11:02:15 +0300

    fix validation SignDto

--- requests/sign_dto.go
+++ requests/sign_dto.go
@@ -1,7 +1,7 @@
 package requests
 
 type SignDto struct {
-	R string `json:"r" validate:"required,validHex64"`
-	S string `json:"s" validate:"required,validHex64"`
-	V int    `json:"v" validate:"required,gte=27,lte=28"`
+	R string `json:"r" validate:"omitempty,validHex64"`
+	S string `json:"s" validate:"omitempty,validHex64"`
+	V int    `json:"v" validate:"omitempty,gte=27,lte=28"`
 }
