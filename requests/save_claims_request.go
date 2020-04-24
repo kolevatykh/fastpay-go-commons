d19d8ea15d34a07a995e01e79913da6889f65a93
a.kuznetsov <a.kuznetsov@solarl.ru>
2020-04-24 12:57:54 +0300

    SaveClaimsRequest add  CurrencyCode

--- requests/save_claims_request.go
+++ requests/save_claims_request.go
@@ -3,5 +3,6 @@ package requests
 import "github.com/SolarLabRU/fastpay-go-commons/models"
 
 type SaveClaimsRequest struct {
-	Claims []models.ClaimsItem `json:"claims" validate:"required,dive"`
+	Claims       []models.ClaimsItem `json:"claims" validate:"required,dive"`
+	CurrencyCode int                 `json:"currencyCode" validate:"required,gte=0,lte=999"`
 }
