95419781913ced88d60c77942f68fb6b5429a6f4
a.kuznetsov <a.kuznetsov@solarl.ru>
2020-05-27 10:28:07 +0300

    fix validation models

--- requests/set_issue_limit_request.go
+++ requests/set_issue_limit_request.go
@@ -2,6 +2,6 @@ package requests
 
 type SetIssueLimitRequest struct {
 	Address      string `json:"address" validate:"required,validHex40"`
-	Value        int64  `json:"value" validate:"required,gt=0"`
+	Value        int64  `json:"value" validate:"gt=0"`
 	CurrencyCode int    `json:"currencyCode" validate:"required,gte=0,lte=999"`
 }
