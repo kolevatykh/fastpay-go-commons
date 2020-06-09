429effc64aafa35eb4aff44266ec9038fb0572c7
a.kuznetsov <a.kuznetsov@solarl.ru>
2020-06-09 12:14:59 +0300

    fix limit request models

--- requests/get_limit_request.go
+++ requests/get_limit_request.go
@@ -2,11 +2,13 @@ package requests
 
 import (
 	"github.com/SolarLabRU/fastpay-go-commons/enums/identity-type-enum"
+	"github.com/SolarLabRU/fastpay-go-commons/enums/juridical-type-enum"
 	"github.com/SolarLabRU/fastpay-go-commons/enums/limit-type-enum"
 )
 
 type GetLimitRequest struct {
-	LimitType    limit_type_enum.LimitType       `json:"limitType" validate:"required,gte=0"`
-	IdentityType identity_type_enum.IdentityType `json:"identityType" validate:"required,gte=0"`
-	CurrencyCode int                             `json:"currencyCode" validate:"required,gte=0,lte=999"`
+	LimitType     limit_type_enum.LimitType         `json:"limitType" validate:"required,gte=0"`
+	IdentityType  identity_type_enum.IdentityType   `json:"identityType" validate:"required,gte=0"`
+	JuridicalType juridical_type_enum.JuridicalType `json:"juridicalType" validate:"required,gte=0"`
+	CurrencyCode  int                               `json:"currencyCode" validate:"required,gte=0,lte=999"`
 }
--- requests/set_limit_request.go
+++ requests/set_limit_request.go
@@ -2,12 +2,14 @@ package requests
 
 import (
 	"github.com/SolarLabRU/fastpay-go-commons/enums/identity-type-enum"
+	"github.com/SolarLabRU/fastpay-go-commons/enums/juridical-type-enum"
 	"github.com/SolarLabRU/fastpay-go-commons/enums/limit-type-enum"
 )
 
 type SetLimitRequest struct {
-	LimitType    limit_type_enum.LimitType       `json:"limitType" validate:"required,gte=0"`
-	IdentityType identity_type_enum.IdentityType `json:"identityType" validate:"required,gte=0"`
-	Value        int64                           `json:"value" validate:"gte=0"`
-	CurrencyCode int                             `json:"currencyCode" validate:"required,gte=0,lte=999"`
+	LimitType     limit_type_enum.LimitType         `json:"limitType" validate:"required,gte=0"`
+	IdentityType  identity_type_enum.IdentityType   `json:"identityType" validate:"required,gte=0"`
+	JuridicalType juridical_type_enum.JuridicalType `json:"juridicalType" validate:"required,gte=0"`
+	CurrencyCode  int                               `json:"currencyCode" validate:"required,gte=0,lte=999"`
+	Value         int64                             `json:"value" validate:"gte=0"`
 }
