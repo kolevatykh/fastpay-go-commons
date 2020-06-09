c03e80bc0f2db0ee0e8ebf91436dacee28e0b964
a.kuznetsov <a.kuznetsov@solarl.ru>
2020-06-09 16:51:41 +0300

    GetAccountLimitsRequest model fix

--- requests/get_account_limits_request.go
+++ requests/get_account_limits_request.go
@@ -1,9 +1,6 @@
 package requests
 
-import "github.com/SolarLabRU/fastpay-go-commons/enums/identity-type-enum"
-
 type GetAccountLimitsRequest struct {
-	IdentityType   identity_type_enum.IdentityType `json:"identityType" validate:"required,gte=0"`
-	CurrencyCode   int                             `json:"currencyCode" validate:"required,gte=0,lte=999"`
-	AccountAddress string                          `json:"accountAddress" validate:"required,validHex40"`
+	CurrencyCode int    `json:"currencyCode" validate:"required,gte=0,lte=999"`
+	Address      string `json:"address" validate:"required,validHex40"`
 }
