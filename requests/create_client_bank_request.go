adb4c45e85e72f1890c9ee0056347fbc007a91ce
a.kuznetsov <a.kuznetsov@solarl.ru>
2020-06-11 17:59:45 +0300

    fix

--- requests/create_client_bank_request.go
+++ requests/create_client_bank_request.go
@@ -5,3 +5,9 @@ type CreateClientBankRequest struct {
 	CountryCode     string            `json:"countryCode"`
 	Params          map[string]string `json:"params"`
 }
+
+func (createClientBankRequest *CreateClientBankRequest) SetDefaults() {
+	if createClientBankRequest.Params == nil {
+		createClientBankRequest.Params = make(map[string]string)
+	}
+}
