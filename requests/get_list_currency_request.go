9de95461c1b235043f68844c209f86c151a3b789
a.kuznetsov <a.kuznetsov@solarl.ru>
2020-08-07 12:53:04 +0300

    add Type for GetListCurrencyRequest and GetPageCurrencyRequest

--- requests/get_list_currency_request.go
+++ requests/get_list_currency_request.go
@@ -5,3 +5,9 @@ import "github.com/SolarLabRU/fastpay-go-commons/enums/currency-type-enum"
 type GetListCurrencyRequest struct {
 	Type currency_type_enum.CurrencyType `json:"type"`
 }
+
+func (getListCurrencyRequest *GetListCurrencyRequest) SetDefaults() {
+	if getListCurrencyRequest.Type == currency_type_enum.Undefined {
+		getListCurrencyRequest.Type = currency_type_enum.DigitalCurrency
+	}
+}
--- requests/get_page_currency_request.go
+++ requests/get_page_currency_request.go
@@ -6,3 +6,9 @@ type GetPageCurrencyRequest struct {
 	GetPageRequest
 	Type currency_type_enum.CurrencyType `json:"type"`
 }
+
+func (getPageCurrencyRequest *GetPageCurrencyRequest) SetDefaults() {
+	if getPageCurrencyRequest.Type == currency_type_enum.Undefined {
+		getPageCurrencyRequest.Type = currency_type_enum.DigitalCurrency
+	}
+}
