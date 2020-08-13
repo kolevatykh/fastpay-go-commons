ddf82304a7ddd97b4392a234b6111540e6c2499d
a.kuznetsov <a.kuznetsov@solarl.ru>
2020-08-13 16:09:22 +0300

    ref

--- requests/reject_application_request.go
+++ requests/reject_application_request.go
@@ -1,9 +1,9 @@
 package requests
 
 type RejectApplicationRequest struct {
-	Id              string  `json:"id" validate:"required,uuid"`
-	AddressAcceptor string  `json:"addressAcceptor" validate:"required,validHex40"`
-	MsgHash         string  `json:"msgHash" validate:"required"`
-	Sig             SignDto `json:"sig" validate:"required,dive"`
-	Exp             int64   `json:"exp" validate:"required"`
+	Id      string  `json:"id" validate:"required,uuid"`
+	Address string  `json:"address" validate:"required,validHex40"`
+	MsgHash string  `json:"msgHash" validate:"required"`
+	Sig     SignDto `json:"sig" validate:"required,dive"`
+	Exp     int64   `json:"exp" validate:"required"`
 }
