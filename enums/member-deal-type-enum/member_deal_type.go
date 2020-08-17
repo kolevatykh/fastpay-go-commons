2db9515d6bc7c22d218f4083cb48d438ad9970c1
a.kuznetsov <a.kuznetsov@solarl.ru>
2020-08-17 17:48:48 +0300

    add Enums

---
+++ enums/member-deal-type-enum/member_deal_type.go
@@ -0,0 +1,28 @@
+package member_deal_type_enum
+
+type MemberDealType int
+
+const (
+	Undefined MemberDealType = iota
+	Participant
+	Arbitrator
+)
+
+func (memberDealType MemberDealType) String() string {
+	return [...]string{"Undefined", "Participant", "Arbitrator"}[memberDealType]
+}
+
+func Parse(memberDealType string) MemberDealType {
+	switch memberDealType {
+	case "Participant":
+		return Participant
+	case "Deposit":
+		return Arbitrator
+	default:
+		return Arbitrator
+	}
+}
+
+func GetAllMemberDealTypes() []MemberDealType {
+	return []MemberDealType{Undefined, Participant, Arbitrator}
+}
---
+++ enums/operation-deal-type-enum/operation_deal_type.go
@@ -0,0 +1,28 @@
+package operation_deal_type_enum
+
+type OperationDealType int
+
+const (
+	Undefined OperationDealType = iota
+	Transfer
+	Deposit
+)
+
+func (operationDealType OperationDealType) String() string {
+	return [...]string{"Undefined", "Transfer", "Deposit"}[operationDealType]
+}
+
+func Parse(operationDealType string) OperationDealType {
+	switch operationDealType {
+	case "Transfer":
+		return Transfer
+	case "Deposit":
+		return Deposit
+	default:
+		return Undefined
+	}
+}
+
+func GetAllOperationDealTypes() []OperationDealType {
+	return []OperationDealType{Undefined, Transfer, Deposit}
+}
