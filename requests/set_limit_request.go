0e9895a529492c41dccabbc8fbc47661ddb57894
a.kuznetsov <a.kuznetsov@solarl.ru>
2020-08-20 15:38:17 +0300

    ref

--- requests/set_limit_request.go
+++ requests/set_limit_request.go
@@ -9,7 +9,7 @@ import (
 type SetLimitRequest struct {
 	LimitType     limit_type_enum.LimitType         `json:"limitType" valid:"required,range(0|9223372036854775807)"`
 	IdentityType  identity_type_enum.IdentityType   `json:"identityType" valid:"required~ErrorIdentityTypeNotPassed,range(0|9223372036854775807)"`
-	JuridicalType juridical_type_enum.JuridicalType `json:"juridicalType" valid:"required~ErrorJuridicalTypeNotPassed,range(0|9223372036854775807)"`
+	JuridicalType juridical_type_enum.JuridicalType `json:"juridicalType" valid:"range(0|9223372036854775807)"`
 	CurrencyCode  int                               `json:"currencyCode" valid:"required~ErrorCurrencyCodeNotPassed,range(0|999)~ErrorCurrencyCodeRange"`
 	Value         int64                             `json:"value" valid:"range(0|9223372036854775807)"`
 }
