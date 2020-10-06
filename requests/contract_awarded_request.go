8df12827faf23f1446cffcf7d45435a33c3b018d
a.kuznetsov <a.kuznetsov@solarl.ru>
2020-10-06 10:57:36 +0300

    ref

--- requests/contract_awarded_request.go
+++ requests/contract_awarded_request.go
@@ -1,7 +1,9 @@
 package requests
 
+import "github.com/SolarLabRU/fastpay-go-commons/models"
+
 type ContractAwardedRequest struct {
 	MakeDepositRequest
-	IsLastDepositNoNeedTransfers bool `json:"isLastDepositNoNeedTransfers"`
-	CurrencyCode                 int  `json:"currencyCode"`
+	IsLastDepositNoNeedTransfers bool                    `json:"isLastDepositNoNeedTransfers"`
+	CurrencyInfo                 models.CurrencyDealInfo `json:"currencyInfo"`
 }
