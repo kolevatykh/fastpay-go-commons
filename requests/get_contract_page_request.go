b63d2c3e787c69b71a6972f5ff2657ec1db68e71
a.kuznetsov <a.kuznetsov@solarl.ru>
2020-10-07 16:07:28 +0300

    ref

--- requests/get_contract_page_request.go
+++ requests/get_contract_page_request.go
@@ -3,6 +3,7 @@ package requests
 import (
 	"github.com/SolarLabRU/fastpay-go-commons/enums/currency-exchange-contracts-type-enum"
 	"github.com/SolarLabRU/fastpay-go-commons/enums/filter-contract-state-enum"
+	"github.com/SolarLabRU/fastpay-go-commons/enums/operation-deal-type-enum"
 )
 
 type GetContractPageRequest struct {
@@ -10,6 +11,7 @@ type GetContractPageRequest struct {
 	Bookmark     string                                                              `json:"bookmark"`
 	EndDate      int64                                                               `json:"endDate" valid:"range(0|9223372036854775807)"`
 	Status       filter_contract_state_enum.FilterContractState                      `json:"status" valid:"range(0|9223372036854775807)"`
+	Condition    operation_deal_type_enum.OperationDealType                          `json:"condition" valid:"range(0|9223372036854775807)"`
 	Address      string                                                              `json:"address" valid:"validHex40~ErrorAddressNotFollowingRegex"`
 	CurrencyCode int                                                                 `json:"currencyCode" valid:"range(0|999)~ErrorCurrencyCodeRange"`
 	Type         currency_exchange_contracts_type_enum.CurrencyExchangeContractsType `json:"type"`
