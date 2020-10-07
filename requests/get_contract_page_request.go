ff2c24f526efbeec60cf8fd03eb02f5a61e6e45c
a.kuznetsov <a.kuznetsov@solarl.ru>
2020-10-07 17:40:00 +0300

    make

--- requests/get_contract_page_request.go
+++ requests/get_contract_page_request.go
@@ -7,14 +7,14 @@ import (
 )
 
 type GetContractPageRequest struct {
-	PageSize     int32                                                               `json:"pageSize" valid:"required"`
-	Bookmark     string                                                              `json:"bookmark"`
-	EndDate      int64                                                               `json:"endDate" valid:"range(0|9223372036854775807)"`
-	Status       filter_contract_state_enum.FilterContractState                      `json:"status" valid:"range(0|9223372036854775807)"`
-	Condition    operation_deal_type_enum.OperationDealType                          `json:"condition" valid:"range(0|9223372036854775807)"`
-	Address      string                                                              `json:"address" valid:"validHex40~ErrorAddressNotFollowingRegex"`
-	CurrencyCode int                                                                 `json:"currencyCode" valid:"range(0|999)~ErrorCurrencyCodeRange"`
-	Type         currency_exchange_contracts_type_enum.CurrencyExchangeContractsType `json:"type"`
+	PageSize      int32                                                               `json:"pageSize" valid:"required"`
+	Bookmark      string                                                              `json:"bookmark"`
+	EndDate       int64                                                               `json:"endDate" valid:"range(0|9223372036854775807)"`
+	Status        filter_contract_state_enum.FilterContractState                      `json:"status" valid:"range(0|9223372036854775807)"`
+	OperationType operation_deal_type_enum.OperationDealType                          `json:"operationType" valid:"range(0|9223372036854775807)"`
+	Address       string                                                              `json:"address" valid:"validHex40~ErrorAddressNotFollowingRegex"`
+	CurrencyCode  int                                                                 `json:"currencyCode" valid:"range(0|999)~ErrorCurrencyCodeRange"`
+	Type          currency_exchange_contracts_type_enum.CurrencyExchangeContractsType `json:"type"`
 }
 
 func (getContractPage *GetContractPageRequest) SetDefaults() {
