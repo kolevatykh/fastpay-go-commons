1773d1f5f17e0a4a49bc5c46ac2a8816532f1fe5
a.kuznetsov <a.kuznetsov@solarl.ru>
2020-10-13 13:44:59 +0300

    ref

--- requests/transfer_batch_request.go
+++ requests/transfer_batch_request.go
@@ -3,3 +3,9 @@ package requests
 type TransferBatchRequest struct {
 	Transfers []TransferRequest `json:"transfers" valid:"required"`
 }
+
+func (transferRequest *TransferBatchRequest) SetDefaults() {
+	for i, _ := range transferRequest.Transfers {
+		transferRequest.Transfers[i].SetDefaults()
+	}
+}
--- requests/transfer_request.go
+++ requests/transfer_request.go
@@ -1,14 +1,23 @@
 package requests
 
+import "github.com/SolarLabRU/fastpay-go-commons/enums/transaction-type-enum"
+
 type TransferRequest struct {
-	AddressFrom   string  `json:"addressFrom" valid:"required~ErrorAddressNotPassed,validHex40~ErrorAddressNotFollowingRegex"`
-	To            string  `json:"to" valid:"required~ErrorAddressNotPassed,validHex40or64~ErrorAddressOrIdentifierNotFolowingRegex"`
-	CurrencyCode  int     `json:"currencyCode" valid:"required~ErrorCurrencyCodeNotPassed,range(0|999)~ErrorCurrencyCodeRange"`
-	Amount        int64   `json:"amount" valid:"required~ErrorAmountNotPassed"`
-	Payload       string  `json:"payload"`
-	MsgHash       string  `json:"msgHash"`
-	Sig           SignDto `json:"sig"`
-	InvoiceNumber string  `json:"invoiceNumber" valid:"maxstringlength(255)~ErrorNumberInvoiceNotFolowingRegex"`
-	Exp           int64   `json:"exp"`
-	TransactionId string  `json:"transactionId" valid:"uuidv4"`
+	AddressFrom   string                                `json:"addressFrom" valid:"required~ErrorAddressNotPassed,validHex40~ErrorAddressNotFollowingRegex"`
+	To            string                                `json:"to" valid:"required~ErrorAddressNotPassed,validHex40or64~ErrorAddressOrIdentifierNotFolowingRegex"`
+	CurrencyCode  int                                   `json:"currencyCode" valid:"required~ErrorCurrencyCodeNotPassed,range(0|999)~ErrorCurrencyCodeRange"`
+	Amount        int64                                 `json:"amount" valid:"required~ErrorAmountNotPassed"`
+	Payload       string                                `json:"payload"`
+	MsgHash       string                                `json:"msgHash"`
+	Sig           SignDto                               `json:"sig"`
+	InvoiceNumber string                                `json:"invoiceNumber" valid:"maxstringlength(255)~ErrorNumberInvoiceNotFolowingRegex"`
+	Exp           int64                                 `json:"exp"`
+	TransactionId string                                `json:"transactionId" valid:"uuidv4"`
+	TxType        transaction_type_enum.TransactionType `json:"txType" valid:"min=0,max=5"`
+}
+
+func (transferRequest *TransferRequest) SetDefaults() {
+	if transferRequest.TxType == transaction_type_enum.Undefined {
+		transferRequest.TxType = transaction_type_enum.Transfer
+	}
 }
