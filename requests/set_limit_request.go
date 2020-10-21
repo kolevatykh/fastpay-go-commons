3cd709eaa86cb14a095856c92a0f993b1da55979
a.kuznetsov <a.kuznetsov@solarl.ru>
2020-10-21 09:43:10 +0300

    ref

--- requests/set_limit_request.go
+++ requests/set_limit_request.go
@@ -2,6 +2,5 @@ package requests
 
 type SetLimitRequest struct {
 	GetLimitRequest
-	CurrencyCode int   `json:"currencyCode" valid:"required~ErrorCurrencyCodeNotPassed,range(0|999)~ErrorCurrencyCodeRange"`
-	Value        int64 `json:"value" valid:"range(0|9223372036854775807)"`
+	Value int64 `json:"value" valid:"range(0|9223372036854775807)"`
 }
