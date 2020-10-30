5cad1b18b2195d2a10fd0ebbd7052eb5d6c89713
a.kuznetsov <a.kuznetsov@solarl.ru>
2020-10-30 15:37:10 +0300

    ref

--- requests/make_deposit_request.go
+++ requests/make_deposit_request.go
@@ -5,7 +5,7 @@ type MakeDepositRequest struct {
 	AddressFrom string  `json:"addressFrom" valid:"required~ErrorAddressNotPassed,validHex40~ErrorAddressNotFollowingRegex"`
 	AddressTo   string  `json:"addressTo" valid:"required~ErrorAddressNotPassed,validHex40~ErrorAddressNotFollowingRegex"`
 	Amount      int64   `json:"amount" valid:"required~ErrorAmountNotPassed,range(0|9223372036854775807)~ErrorAmountNegative"`
-	MsgHash     string  `json:"msgHash" valid:"required"`
-	Sig         SignDto `json:"sig" valid:"required"`
-	Exp         int64   `json:"exp" valid:"required~ErrorTimestampNotPassed"`
+	MsgHash     string  `json:"msgHash"`
+	Sig         SignDto `json:"sig"`
+	Exp         int64   `json:"exp"`
 }
