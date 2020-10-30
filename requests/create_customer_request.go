c7eb18f660bbf484ef763e057d6097818692a699
v.mosenkov <v.mosenkov@solarl.ru>
2020-10-30 10:12:13 +0300

    Добавил в запросы техническую подпись

--- requests/create_customer_request.go
+++ requests/create_customer_request.go
@@ -1,6 +1,7 @@
 package requests
 
 type CreateCustomerRequest struct {
+	TechnicalSignRequest
 	Identifier          string `json:"identifier" valid:"required~ErrorIdentifierNotPassed,validHex64~ErrorIdentifierNotFolowingRegex"`
 	CustomerDisplayName string `json:"customerDisplayName"`
 	BankId              string `json:"bankId" valid:"required~ErrorBankIdNotPassed"`
--- requests/update_jurudical_request.go
+++ requests/update_jurudical_request.go
@@ -3,6 +3,7 @@ package requests
 import juridical_type_enum "github.com/SolarLabRU/fastpay-go-commons/enums/juridical-type-enum"
 
 type UpdateJuridicalRequest struct {
+	TechnicalSignRequest
 	Address       string                            `json:"address" valid:"required~ErrorAddressNotPassed,validHex40~ErrorAddressNotFollowingRegex"`
 	JuridicalType juridical_type_enum.JuridicalType `json:"juridicalType" valid:"required~ErrorJuridicalTypeNotPassed"`
 }
