5269101bc58142879c682d073e5b123fa37e91c2
v.mosenkov <v.mosenkov@solarl.ru>
2020-11-02 12:37:48 +0300

    Добавил техническую подпись в операции списания зачисления

--- requests/withdraw_confirm_request.go
+++ requests/withdraw_confirm_request.go
@@ -1,6 +1,7 @@
 package requests
 
 type WithdrawConfirmRequest struct {
+	TechnicalSignRequest
 	AddressFrom   string `json:"addressFrom" valid:"required~ErrorAddressNotPassed,validHex40~ErrorAddressNotFollowingRegex"`
 	TxId          string `json:"txId" valid:"required~ErrorTxIdSNotPassed"`
 	CurrencyCode  int    `json:"currencyCode" valid:"required~ErrorCurrencyCodeNotPassed,range(0|999)~ErrorCurrencyCodeRange"`
