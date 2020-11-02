710ef3390a19d22a778a27ac07f7dec210ace497
v.mosenkov <v.mosenkov@solarl.ru>
2020-11-02 20:17:03 +0300

    Поправил реквест на создание customer

--- requests/create_customer_request.go
+++ requests/create_customer_request.go
@@ -4,5 +4,5 @@ type CreateCustomerRequest struct {
 	TechnicalSignRequest
 	Identifier          string `json:"identifier" valid:"required~ErrorIdentifierNotPassed,validHex64~ErrorIdentifierNotFolowingRegex"`
 	CustomerDisplayName string `json:"customerDisplayName"`
-	Address             string `json:"address" valid:"required~ErrorBankAddressNotPassed"`
+	BankAddress         string `json:"bankAddress" valid:"required~ErrorBankAddressNotPassed"`
 }
